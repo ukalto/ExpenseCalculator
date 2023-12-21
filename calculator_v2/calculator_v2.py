d2143b430a43f868b52257740a0cc74674df1c76
ukalto <maxi.gaber@gmail.com>
2023-12-21 23:06:03 +0100

    updated

--- calculator_v2/calculator_v2.py
+++ calculator_v2/calculator_v2.py
@@ -4,12 +4,13 @@ from datetime import datetime
 origin_path = "Files/"
 
 
-def print_all_files(path):
+def print_all_files(path, exclude):
     general_expenses = []
     for file in os.listdir(path):
-        sum_expenses = process_input(path + "/" + file)
-        print(f"{file}: {sum_expenses}€")
-        general_expenses.append(sum_expenses)
+        if not file in exclude:
+            sum_expenses = process_input(path + "/" + file)
+            print(f"{file}: {sum_expenses}€")
+            general_expenses.append(sum_expenses)
     print(f"General: {round(sum(general_expenses), 2)}€")
 
 
@@ -33,13 +34,22 @@ def continue_asking():
             return False
 
 
+def check_action():
+    while True:
+        check = input("Sure you want to do this? Y/N")
+        if check == 'Y' or check == 'y':
+            return True
+        elif check == 'N' or check == 'n':
+            return False
+
+
 def option_one():
     directories = os.listdir(origin_path)
     directories.remove(".gitkeep")
     directories.sort(key=lambda date: datetime.strptime(date, "%b.%Y"))
     for i in directories:
         print(i + "\n")
-        print_all_files(origin_path + i)
+        print_all_files(origin_path + i, "")
         print("---------------")
 
 
@@ -50,13 +60,13 @@ def option_two():
             path = origin_path + directory
             if os.path.exists(path):
                 break
-        print_all_files(path)
+        print_all_files(path, "")
         if not continue_asking():
             break
 
 
 def option_three():
-    files = ["auto.txt", "bekommeich.txt", "bestellen.txt", "e-scooter.txt", "einkauf.txt", "essen.txt", "selber.txt"]
+    files = open('files_preset', 'r').read().split('\n')
     while True:
         directory_name = input("Directory-Name-Format: Mon.YYYY\n")
         try:
@@ -87,8 +97,84 @@ def option_four():
             continue
 
 
+def option_five():
+    directories = os.listdir(origin_path)
+    directories.remove(".gitkeep")
+    final_sum = 0
+    for directory in directories:
+        path = os.path.join(origin_path, directory)
+        file_path = os.path.join(path, "bekommeich.txt")
+        if os.path.exists(file_path):
+            sum_expenses = process_input(file_path)
+            final_sum += sum_expenses
+            print(f"{directory}: {sum_expenses}€")
+    print(f"Sum of currently open payments: {final_sum}€\n")
+
+
+def option_six():
+    if not check_action():
+        return
+    directories = os.listdir(origin_path)
+    directories.remove(".gitkeep")
+    for directory in directories:
+        try:
+            file_path = os.path.join(origin_path, directory)
+            if os.path.exists(file_path + "/bekommeich.txt"):
+                os.rename(file_path + "/bekommeich.txt", file_path + "/bekommeich_payed.txt")
+        except ValueError:
+            continue
+        except FileNotFoundError:
+            continue
+
+
+def option_seven():
+    directories = os.listdir(origin_path)
+    directories.remove(".gitkeep")
+    directories.sort(key=lambda date: datetime.strptime(date, "%b.%Y"))
+    for i in directories:
+        print(i + "\n")
+        print_all_files(origin_path + i, "investments.txt")
+        print("---------------")
+
+
+def option_eight():
+    directories = os.listdir(origin_path)
+    directories.remove(".gitkeep")
+    directories.sort(key=lambda date: datetime.strptime(date, "%b.%Y"))
+    for i in directories:
+        print(i + "\n")
+        general_expenses = []
+        for file in os.listdir(origin_path + i):
+            if file == "investments.txt":
+                sum_expenses = process_input(origin_path + i + "/" + file)
+                print(f"{file}: {sum_expenses}€")
+                general_expenses.append(sum_expenses)
+        print(f"General: {round(sum(general_expenses), 2)}€")
+        print("---------------")
+
+
+def option_nine():
+    directories = os.listdir(origin_path)
+    directories.remove(".gitkeep")
+    directories.sort(key=lambda date: datetime.strptime(date, "%b.%Y"))
+    for i in directories:
+        print(i + "\n")
+        print_all_files(origin_path + i, ["investments.txt", "bekommeich.txt", "bekommeich_payed.txt"])
+        print("---------------")
+
+
 def execute_options():
-    option = input(f'Type 1 2 3 4\nOption 1: List all Expenses\nOption 2: Show specific File\nOption 3: Add new File\nOption 4: Mark payed\n')
+    option = input(
+        f'Type 1 2 3 4 5 6 7 8 9\n'
+        f'Option 1: List all Expenses\n'
+        f'Option 2: Show specific File\n'
+        f'Option 3: Add new File\n'
+        f'Option 4: Mark payed\n'
+        f'Option 5: Calculate open payments\n'
+        f'Option 6: Mark all payed\n'
+        f'Option 7: List all expenses without investments\n'
+        f'Option 8: List all investments\n'
+        f'Option 9: List all expenses without investments and without money back\n')
     if option == '1':
         option_one()
     elif option == '2':
@@ -97,6 +183,16 @@ def execute_options():
         option_three()
     elif option == '4':
         option_four()
+    elif option == '5':
+        option_five()
+    elif option == '6':
+        option_six()
+    elif option == '7':
+        option_seven()
+    elif option == '8':
+        option_eight()
+    elif option == '9':
+        option_nine()
 
 
 def main():
